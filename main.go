6c4c1b67d7a147c34255baf345d3214a2741117c
Tomasz Klara <tomaszklara@gmail.com>
2021-11-03 22:35:46 +0000

    updates

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	_, err := w.Write([]byte("Hello from record app"))
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
 func main() {
-	fmt.Println("Hello")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", Home)
+
+	log.Println("Starting server on :4000")
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
 }
